983063ef44aed08181452fede2be2ad02eb722ce
irsandymsv <irsandymsv98@gmail.com>
2019-10-09 11:10:50 +0700

    memperbarui hal create-form sk skripsi

---
+++ resources/views/akademik/Skripsi/show.blade.php
@@ -0,0 +1,13 @@
+@extends('akademik.akademik_view')
+
+@section('judul_header')
+	SK Skripsi
+@endsection
+
+@section('content')
+	<div class="row">
+		<div class="">
+			
+		</div>
+	</div>
+@endsection
