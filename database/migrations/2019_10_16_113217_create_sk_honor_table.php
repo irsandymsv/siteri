1c0f0196b64dab9697d4d57071dc27bb0a1f785a
[FadhilM] <fadhilm614@gmail.com>
2019-10-16 17:35:29 +0700

    ada yang kelupaan wkwkwkw

--- database/migrations/2019_10_16_113217_create_sk_honor_table.php
+++ database/migrations/2019_10_16_113217_create_sk_honor_table.php
@@ -16,16 +16,17 @@ class CreateSkHonorTable extends Migration
         Schema::create('sk_honor', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedTinyInteger('id_tipe_sk')->nullable();
-            $table->unsignedInteger('id_status_sk_akademik')->nullable();
-            $table->boolean('verif_wadek')->default(false);
+            $table->unsignedInteger('id_status_sk_honor')->nullable();
+            $table->unsignedBigInteger('honor_pembimbing');
+            $table->unsignedBigInteger('honor_penguji');
             $table->boolean('verif_ktu')->default(false);
             $table->boolean('verif_kebag_keuangan')->default(false);
+            $table->boolean('verif_wadek2')->default(false);            
             $table->boolean('verif_dekan')->default(false);
             $table->text('pesan_revisi')->nullable();
             $table->timestamps();
 
             $table->foreign('id_tipe_sk')->references('id')->on('tipe_sk')->onDelete('set null');
-            $table->foreign('id_status_sk_akademik')->references('id')->on('status_sk_akademik')->onDelete('set null');
 
         });
     }
