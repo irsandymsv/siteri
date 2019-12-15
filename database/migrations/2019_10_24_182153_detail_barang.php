80d83bf23c3eb3463c6672664578be34b883c553
devinrry <devi.alannurry27@gmail.com>
2019-12-15 10:21:05 +0700

    update data  detail barang

--- database/migrations/2019_10_24_182153_detail_barang.php
+++ database/migrations/2019_10_24_182153_detail_barang.php
@@ -14,11 +14,11 @@ class DetailBarang extends Migration
     public function up()
     {
         Schema::create('detail_barang', function (Blueprint $table) {
-        //     $table->increments('id');
-        //     $table->unsignedInteger('id_categori');
-        //     $table->string('spesifikasi_barang');
-        //     $table->bigInteger('harga_satuan');
-        // });
+            $table->increments('id');
+            $table->unsignedInteger('id_categori');
+            $table->string('spesifikasi_barang');
+            $table->bigInteger('harga_satuan');
+        });
     }
 
     /**
